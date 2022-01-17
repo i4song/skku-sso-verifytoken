043dc2724e07015e072796577ad97a1e4ce351d5
yongwookLee <yongw00k@g.skku.edu>
2022-01-18 06:04:23 +0900

    refactor: Move license informations into application.properties

--- src/main/java/edu/skku/ssopackage/RESTApi.java
+++ src/main/java/edu/skku/ssopackage/RESTApi.java
@@ -1,33 +1,25 @@
 package edu.skku.ssopackage;
 
 import SafeIdentity.SSO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 @RestController
 public class RESTApi {
+    @Value("${sApiKey}")
+    String sApiKey;
+    @Value("${ssoAgentIp}")
+    String ssoAgentIp;
+    @Value("${ssoAgentPort}")
+    Integer ssoAgentPort;
+
     @PostMapping("/verifyToken")
-    public int verifyToken(@RequestBody String ssoToken) throws IOException {
-        String propFile = "licence.txt";
-        Properties props = new Properties();
-
-        FileInputStream fis = new FileInputStream(propFile);
-
-        props.load(new java.io.BufferedInputStream(fis));
-
-        String sApiKey = props.getProperty("sApiKey");
-        String ssoAgentIp = props.getProperty("ssoAgentIp");
-        String ssoAgentPort = props.getProperty("ssoAgentPort");
-
+    public int verifyToken(@RequestBody String ssoToken) {
         SSO sso = new SSO(sApiKey);
-
         sso.setHostName(ssoAgentIp);
-        sso.setPortNumber(Integer.parseInt(ssoAgentPort));
+        sso.setPortNumber(ssoAgentPort);
 
         return sso.verifyToken(ssoToken);
     }
